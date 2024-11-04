9b3e9354925760da3382ac3c93f58e3549e71ec0
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-11-04 06:10:09 +0300

    feat: добавил новые приложения

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -25,12 +25,14 @@ SECRET_KEY = 'django-insecure-o*n=_kwi5zmtx8w7ej__8&9)oyw=zqrt@2^owwpp7&66egm7^%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
